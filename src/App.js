8d1e3c14094d59b3d244ef02f272efea07727858
ISLAMBEK <93197300+islambek97@users.noreply.github.com>
2023-10-19 17:10:37 +0600

    props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, {useState} from "react";
+import Counter from "./components/Counter";
+import './styles/App.css'
+import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
 
 function App() {
-    const [likes, setLikes] = useState(5)
-    const [value, setValue] = useState('ISLAMBEK')
-    function increment() {
-        setLikes(likes + 1)
-    }
 
-    function decrement() {
-        setLikes(likes - 1)
-    }
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'JavaScript', body: 'Description'},
+        {id: 2, title: 'JavaScript 2', body: 'Description'},
+        {id: 3, title: 'JavaScript 3', body: 'Description'},
+    ])
+
+    const [posts2, setPosts2] = useState([
+        {id: 1, title: 'Python', body: 'Description'},
+        {id: 2, title: 'Python 2', body: 'Description'},
+        {id: 3, title: 'Python 3', body: 'Description'},
+    ])
 
     return (
         <div className="App">
-            <h1>{likes}</h1>
-            <h1>{value}</h1>
-            <button onClick={increment}> Increment</button>
-            <button onClick={decrement}> Decrement</button>
-            <input type="text"
-                   value={value}
-                    onChange={event => setValue(event.target.value)}
-            />
+            <PostList posts={posts} title={"JS Posts list"}/>
+            <PostList posts={posts} title={"Python Posts list"}/>
         </div>
     );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Counter = () => {
+const Counter = function (){
+    //Это хуки
+    const [count, steCount] = useState(0)
+
+    function increment() {
+        steCount(count + 1)
+    }
+
+    function decrement() {
+        steCount(count - 1)
+    }
+
     return (
         <div>
-            
+            <h1>{count}</h1>
+            <button onClick={increment}> Increment</button>
+            <button onClick={decrement}> Decrement</button>
         </div>
-    );
-};
-
+    )
+}
 export default Counter;
